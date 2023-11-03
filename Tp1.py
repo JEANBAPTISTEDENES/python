e152e6ebc2e80522746d45b1486a049d00b66d66
JEANBAPTISTEDENES <jeanbaptistedenes2@gmail.com>
2023-11-03 12:38:22 -0400

    commit tp1

---
+++ Tp1.py
@@ -0,0 +1,31 @@
+
+#mete yon chen karacte an miniskil
+mo="HELLO Guys ,Wellcome Back"
+
+m=mo.lower()
+print(m )
+print()
+
+# nimewo 2
+nom="Ayibobo ayiti"
+rep=nom.split()
+print(rep)
+print()
+
+#nimero 3
+rep2=nom.title()
+print(rep2)
+print()
+
+# nimewo 4rep4=rep[0]print(rep4)
+
+#nimewo 5
+rep4=rep2.replace("A" ,"@")
+print(rep4)
+print()
+
+#nimewo 7
+rep5="".join(reversed(nom))
+rep6=rep5.upper()
+
+print(rep6)
