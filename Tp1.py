46e3cd615e97050a2d0bcb431d057931d6e9d0c6
JEANBAPTISTEDENES <jeanbaptistedenes2@gmail.com>
2023-11-05 20:52:49 -0500

    mod 5

--- Tp1.py
+++ Tp1.py
@@ -33,10 +33,32 @@ rep5="".join(reversed(nom))
 rep6=rep5.upper()
 
 print(rep6)
+print()
 
 
 # nimewo 7
 index=mo.index("a")
 print(index)
+print()
 
-# nimero 8
+# nimero 8 e 9
+chenn = "Ayiti kapab avanse"
+
+index_a = []
+for i, let in enumerate(chenn):
+    if let.lower() == "a":
+        index_a.append(i)
+
+som_index_a = sum(index_a)
+
+print("Les indexes de 'a' sont:", index_a)
+print("La somme des indexes de 'a' est:", som_index_a)
+print()
+
+# nimewo 10
+
+chenn_san_espas = chenn.replace(" ", "")
+kantite_karakte_sans_espas = len(chenn_san_espas)
+
+print("Chenn san espas:", chenn_san_espas)
+print("Kantite karaktè san espas:", kantite_karakte_sans_espas)
